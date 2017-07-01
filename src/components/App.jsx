48089eb4f393bcecdaec2df7c0528c2f83c8ff28
Kamyl <kamillcybulski@gmail.com>
2017-07-02 00:56:37 +0200

    Initial commit. Setup

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Nav from './Nav';
+import About from './About';
+import Work from './Work';
+import Contact from './Contact';
+import Bottom from './Bottom';
+
+const App = () => (
+  <div>
+    <Nav />
+    <About />
+    <Work />
+    <Contact />
+    <Bottom />
+  </div>
+);
+
+
+
+export default App;
---
+++ src/components/Bottom.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Bottom = () => (
+  <div>Bottom</div>
+)
+
+export default Bottom;
